36ef63a3ac1ad0cbb7040c018caf0e776886e2a5
ziad501 <ziadmuhammed501@gmail.com>
2025-03-17 06:32:57 +0200

    add form validation and add some decorators to category.cs

--- Elite.Presentation/Controllers/CategoryController.cs
+++ Elite.Presentation/Controllers/CategoryController.cs
@@ -16,9 +16,35 @@ namespace Elite.Presentation.Controllers
             List<Category> categories = _context.Categories.ToList();
             return View(categories);
         }
-        public IActionResult Create()
+        public IActionResult CreateCategory() => View("CreateCategory");
+        [HttpPost]
+        public IActionResult SaveCategory(Category cat)
         {
-            return View();
+            if (!ModelState.IsValid)
+            {
+                return View("CreateCategory", cat);
+            }
+            if(cat.Name == cat.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "Category Name and Display Order cannot be the same.");
+                return View("CreateCategory", cat);
+            }
+            if (_context.Categories.Any(c => c.Name == cat.Name))
+            {
+                ModelState.AddModelError("Name", "Category Name already exists");
+                return View("CreateCategory", cat);
+            }
+            if (_context.Categories.Any(p => p.DisplayOrder == cat.DisplayOrder))
+            {
+                ModelState.AddModelError("DisplayOrder", "Display Order is required and must be unique.");
+                return View("CreateCategory", cat);
+            }
+            else
+            {
+                _context.Categories.Add(cat);
+                _context.SaveChanges();
+                return RedirectToAction("Category", cat);
+            }
         }
     }
 }
--- Elite.Presentation/Models/Category.cs
+++ Elite.Presentation/Models/Category.cs
@@ -5,10 +5,12 @@ namespace Elite.Presentation.Models
     public class Category
     {
         public int ID { get; set; }
-        [Required]
+        [Required(ErrorMessage ="Name shouldn't be Empty")]
+        [StringLength(20, MinimumLength = 3,ErrorMessage ="Name must be between 3-20 character")]
         [Display(Name = "Category Name")]
-        public string Name { get; set; }
+        public string Name { get; set; }= string.Empty;
         [Display(Name = "Display Order")]
+        [Range(1,100, ErrorMessage = "Display Order must be between 1 and 100")]
         public int DisplayOrder { get; set; }
     }
 }
